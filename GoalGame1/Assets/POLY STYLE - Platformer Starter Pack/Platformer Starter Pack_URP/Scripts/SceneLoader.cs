7eb94575eb13b935d4b2266843d816b962e2ca10
shiongongshou1 <shiongongshou1@gmail.com>
2025-09-04 00:48:07 +0900

    StartScene

---
+++ GoalGame1/Assets/POLY STYLE - Platformer Starter Pack/Platformer Starter Pack_URP/Scripts/SceneLoader.cs
@@ -0,0 +1,11 @@
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class SceneLoader : MonoBehaviour
+{
+    // PlayerSelectƒV[ƒ“‚ÖˆÚ“®
+    public void LoadPlayerSelect()
+    {
+        SceneManager.LoadScene("PlayerSelectScene");
+    }
+}
